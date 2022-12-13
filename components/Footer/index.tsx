96b78ca67b4334f8f4fd8dc57609a2164361f2fe
ggsingla <ggsingla777@gmail.com>
2022-12-13 13:10:22 +0530

    Optimized for build

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,13 +1,19 @@
 import { Copyright } from './Copyright'
 import { SocialLinks } from './SocialLinks'
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <footer aria-label='Site Footer' className='bg-[#C1FFF0] mt-10'>
       <div className='mx-auto max-w-5xl px-4  py-6 sm:px-6 lg:px-8'>
         <div className='flex justify-center text-teal-600'>
-          <img src='/logo/cv-logo.png' alt='cv logo' />
+          <Image
+            height={200}
+            width={200}
+            src='/logo/cv-logo.png'
+            alt='cv logo'
+          />
         </div>
         <h2 className='text-4xl text-center font-bold'>Carrer Vyas</h2>
         <h2 className='md:text-3xl text-xl text-center text-dark/70 font-bold'>
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
     <>
       <header className='bg-gradient-to-b from-accent/50 to-white'>
         <div className='mx-auto container flex justify-between pt-5 h-20 items-center gap-8 px-4 sm:px-6 lg:px-8'>
-          <a className='block text-teal-600' href='/'>
+          <Link className='block text-teal-600' href='/'>
             <span className='sr-only'>Home</span>
             <Image
               height={100}
@@ -14,12 +15,12 @@ const Navbar = () => {
               src='/logo/cv-logo.png'
               alt='carrer vyas logo'
             />
-          </a>
-          <a
+          </Link>
+          <Link
             className='block rounded-md bg-accent px-5 py-2.5 text-sm font-medium text-white transition hover:bg-accent/80'
             href='/'>
             Subscribe
-          </a>
+          </Link>
         </div>
       </header>
     </>
